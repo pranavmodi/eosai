b9269f5c961de67e07a85807fca71f1907e9ffe2
Pranav <pranav.modi@gmail.com>
2025-04-25 16:39:44 +0530

    update code

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
+import netlify from '@astrojs/netlify';
 
 export default defineConfig({
+  output: 'server',
+  adapter: netlify(),
   integrations: [tailwind()]
 });
